20e0f9f982ec04a8c5bbf0f5fa57557df66ead49
Ali Gorji <ag.aligorji@gmail.com>
2022-08-11 11:42:16 +0430

    create useWatchQuery composable

---
+++ src/composables/useWatchQuery.ts
@@ -0,0 +1,34 @@
+import { watch } from 'vue';
+import {
+  LocationQuery,
+  LocationQueryRaw,
+  useRoute,
+  useRouter,
+} from 'vue-router';
+
+export default function useWatchQuery(
+  onChangeQuery: (value: LocationQuery) => void
+) {
+  const route = useRoute();
+  const router = useRouter();
+
+  function setQuery(query: LocationQueryRaw, type: 'replace' | 'add') {
+    let newQuery;
+    if (type === 'replace') {
+      newQuery = query;
+    } else if (type === 'add') {
+      newQuery = { ...route.query, ...query };
+    }
+    router.replace({ query: newQuery });
+  }
+
+  watch(
+    () => route.query,
+    (value) => {
+      onChangeQuery(value);
+    },
+    { immediate: true, deep: true }
+  );
+
+  return { setQuery, currentQuery: route.query };
+}
