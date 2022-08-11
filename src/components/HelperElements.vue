d811cc0fd48de7e8a079cde222c595a73fbafc83
Ali Gorji <ag.aligorji@gmail.com>
2022-08-11 16:41:05 +0430

    create helperElement component

---
+++ src/components/HelperElements.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+defineProps<{
+  loading: boolean;
+  error: null | string;
+  moviesLength: number;
+}>();
+</script>
+
+<template>
+  <div class="helperElements">
+    <p v-if="loading">Loading ...</p>
+    <p v-else-if="error" class="helperElements__errorMsg">{{ error }} 🚨</p>
+    <p v-else>No Data, please search with another parameters 👆</p>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.helperElements {
+  font-size: 1.5rem;
+  text-align: center;
+  margin-top: 3rem;
+
+  &__errorMsg {
+    color: orangered;
+  }
+}
+</style>
