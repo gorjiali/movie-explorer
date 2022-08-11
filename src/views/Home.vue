cb60abc0fc4a547ce221bb02bc6cef743f0cd59b
Ali Gorji <ag.aligorji@gmail.com>
2022-08-11 16:41:27 +0430

    add helperElements and useFetchMovies to home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,42 +1,50 @@
 <script setup lang="ts">
-import { ref, watch } from 'vue';
-import { MovieItem, QueryKeys } from '@/types';
-import { fetchMovies } from '@/api';
+import { ref } from 'vue';
 import useWatchQuery from '@/composables/useWatchQuery';
+import useFetchMovies from '@/composables/useFetchMovies';
 import MovieList from '@/components/MovieList.vue';
 import Pagination from '@/components/Pagination.vue';
+import FilterForm from '@/components/FilterForm.vue';
+import { LocationQueryRaw } from 'vue-router';
+import HelperElements from '@/components/HelperElements.vue';
 
+const { getMovieList, loading, error, movies, totalPages } = useFetchMovies();
 const { currentQuery, setQuery } = useWatchQuery(getMovieList);
 
-const queriedData = ref<MovieItem[]>([]);
-const totalPages = ref<number>(0);
 const page = ref(Number(currentQuery.page) || 1);
-
-watch(page, (newPage) => {
-  setQuery({ page: newPage }, 'add');
-});
-
-function getMovieList(value: QueryKeys) {
-  fetchMovies(value).then(({ results, total_pages }) => {
-    queriedData.value = results;
-    totalPages.value = total_pages <= 500 ? total_pages : 500; // because of api limitation
-  });
-}
 
 function handleChangePage(newPage: number) {
   page.value = newPage;
+  setQuery({ page: newPage }, 'add');
+}
+
+function handleChangeFilter(filters: LocationQueryRaw) {
+  page.value = 1;
+  setQuery({ page: 1, ...filters }, 'replace');
 }
 </script>
 
 <template>
   <div class="container">
-    <MovieList ref="dataTableRef" :list="queriedData" />
-    <Pagination
-      :currentPage="page"
-      :totalPages="totalPages"
-      :itemsPerPage="20"
-      @onChangePage="handleChangePage"
+    <FilterForm
+      :currentQuery="currentQuery"
+      @onChangeFilter="handleChangeFilter"
     />
+    <HelperElements
+      v-if="loading || error || !movies.length"
+      :moviesLength="movies.length"
+      :loading="loading"
+      :error="error"
+    />
+    <div v-else>
+      <MovieList ref="dataTableRef" :list="movies" />
+      <Pagination
+        :currentPage="page"
+        :totalPages="totalPages"
+        :itemsPerPage="20"
+        @onChangePage="handleChangePage"
+      />
+    </div>
   </div>
 </template>
 
